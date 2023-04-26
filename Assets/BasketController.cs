8f25d4afa9659c9cedc7d5cc57f2ab05be5351b5
magu-halfmat <gamemnhr@gmail.com>
2023-04-26 22:52:39 +0900

    リンゴと爆弾にタグつけた

--- Assets/BasketController.cs
+++ Assets/BasketController.cs
@@ -9,6 +9,20 @@ public class BasketController : MonoBehaviour
         Application.targetFrameRate = 60;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Apple")
+        {
+            Debug.Log("Tag = Apple"); 
+        }
+
+        else
+        {
+            Debug.Log("Tag = Bomb");
+        }        
+        Destroy(other.gameObject);
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
